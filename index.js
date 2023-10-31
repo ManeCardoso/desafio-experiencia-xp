616ad8fc8bc68a05546782838e4b11068763051c
Mayane Cardoso <mayanecardoso26@outlook.com>
2023-10-31 11:12:24 -0300

    git remote add origin https://github.com/ManeCardoso/desafio-experiencia-xp.git

---
+++ index.js
@@ -0,0 +1,22 @@
+nome = input("Kitana")
+xp = int(input("6000"))
+
+
+if xp < 1000:
+    nivel = "Ferro"
+else if 1001 <= xp <= 2000:
+    nivel = "Bronze"
+else if 2001 <= xp <= 5000:
+    nivel = "Prata"
+else if 6001 <= xp <= 7000:
+    nivel = "Ouro"
+else if 7001 <= xp <= 8000:
+    nivel = "Platina"
+else  if 8001 <= xp <= 9000:
+    nivel = "Ascendente"
+else if 9001 <= xp <= 10000:
+    nivel = "Imortal"
+else:
+    nivel = "Radiante"
+
+print(f"O Herói de nome "nome" está no nível de "xp")
